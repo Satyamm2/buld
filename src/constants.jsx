5daeae412e6d366db006ba1b2492e4693bfc3cd3
unknown <smsatyam28@gmail.com>
2025-01-02 10:32:19 +0530

    api url update

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL = `${import.meta.env.VITE_API_URL}`;
 export const CUST_API_URL = `${API_URL}/api/customer/init`;
 export const BRAND_NAME = "Buld";
